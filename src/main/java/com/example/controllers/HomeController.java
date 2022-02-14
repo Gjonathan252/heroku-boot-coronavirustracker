505a3d65b066316ebbc77b1abbbb64ebab174cca
Gjonathan252 <g.jonathan252@gmail.com>
2022-02-14 00:59:19 -0800

    Services, Models, Controllers

---
+++ src/main/java/com/example/controllers/HomeController.java
@@ -0,0 +1,29 @@
+package com.example.controllers;
+
+import com.example.models.LocationStats;
+import com.example.services.US_CoronaVirusDataServices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    US_CoronaVirusDataServices coronaVirusDataService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(LocationStats::getLatestTotalCases).sum();
+        int totalNewCases = allStats.stream().mapToInt(LocationStats::getDiffFromPrevDay).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "index";
+    }
+}
